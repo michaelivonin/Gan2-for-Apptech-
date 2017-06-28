ee204966ef8380229c7f93a76bcfbd3d479a0877
Michael Ivonin <michael.ivonin@gmail.com>
2017-06-28 18:55:08 +0600

    Header Done

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Mike on 28.06.2017.
+ */
+
+'use strict';
+
+$(document).ready(function() {
+  /*$('.header__button').click(function() {
+    if ($(window).width() <= 743) {
+      $('.header__cart, .cart').css({'display': 'inline-flex', 'height': '60px'});
+    }
+  });*/
+});
